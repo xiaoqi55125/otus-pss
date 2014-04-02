166cf605a89a7e5b7bb8e232739150e0d0edfd80
Zhicheng Liu <xiaoqi55125@gmail.com>
2014-04-02 23:37:51 +0800

    add tips

--- public/javascript/inventory.js
+++ public/javascript/inventory.js
@@ -17,26 +17,31 @@ function getAllInventories () {
 			if (data.statusCode === 0) {
 				var invs = data.data;
 				$("#add_listView").html("");
-				for (var i = 0; i < invs.length; i++) {
-					var iInfo = invs[i];
-					var row = tdCont.row();
-					var pName = tdCont.cell(iInfo.PRODUCT_NAME);
-					var num = tdCont.cell(iInfo.NUM);
-					var price = tdCont.cell(iInfo.PRICE);
-					var manName = tdCont.cell(iInfo.MANUFACTURE_NAME);
-					// var manDate ;
-					// if (moment(iInfo.MANUFACTURE_DATE).isValid()) {
-					// 	manDate = tdCont.cell(moment(iInfo.MANUFACTURE_DATE).format("YYYY年 M月 D日 H:mm:ss"));
-					// }else{
-					// 	manDate = tdCont.cell('无');
-					// }
-					row.append(pName);
-					row.append(num);
-					row.append(price);
-					row.append(manName);
-					//row.append(manDate);
-					$("#add_listView").append(row);
-				};
+				if (invs.length) {
+					for (var i = 0; i < invs.length; i++) {
+						var iInfo = invs[i];
+						var row = tdCont.row();
+						var pName = tdCont.cell(iInfo.PRODUCT_NAME);
+						var num = tdCont.cell(iInfo.NUM);
+						var price = tdCont.cell(iInfo.PRICE);
+						var manName = tdCont.cell(iInfo.MANUFACTURE_NAME);
+						// var manDate ;
+						// if (moment(iInfo.MANUFACTURE_DATE).isValid()) {
+						// 	manDate = tdCont.cell(moment(iInfo.MANUFACTURE_DATE).format("YYYY年 M月 D日 H:mm:ss"));
+						// }else{
+						// 	manDate = tdCont.cell('无');
+						// }
+						row.append(pName);
+						row.append(num);
+						row.append(price);
+						row.append(manName);
+						//row.append(manDate);
+						$("#add_listView").append(row);
+					};
+				}else{
+					$("#add_listView").html("<h4>暂无记录!</h4>");
+				}
+				
 			}
 		}
 	})
