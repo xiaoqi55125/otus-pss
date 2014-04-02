fd96e78b2a3addabbda7555b4b29725502f898b9
Zhicheng Liu <xiaoqi55125@gmail.com>
2014-04-02 11:46:40 +0800

    add limit to group

--- public/javascript/group.js
+++ public/javascript/group.js
@@ -1,3 +1,21 @@
+// 定义一个判断函数
+var in_array = function(arr){
+
+    // 遍历是否在数组中
+    for(var i=0,k=arr.length;i<k;i++){
+        if(this==arr[i]){
+            return true;    
+        }
+    }
+ 
+    // 如果不在数组中就会返回false
+    return false;
+}
+ 
+// 给字符串添加原型
+String.prototype.in_array = in_array;
+// 给数字类型添加原型
+Number.prototype.in_array = in_array;
 
 function getAllUsers () {
 	$.ajax({
@@ -16,23 +34,61 @@ function getAllUsers () {
 	})
 }
 
-function getAllGroups () {
+function getAllGroups (uId) {
 	$.ajax({
 		url:'securitygroups',
 		type:'get',
+		async   : false,
 		success:function (data) {
 			if (data.statusCode === 0 ) {
-				for (var i = data.data.length - 1; i >= 0; i--) {
-					var group = data.data[i];
-					var temp = "<option value='" + group.GROUP_ID + "'>" + group.DESCRIPTION + "</option>";
-					$("#groupSelect").append(temp);
-				};
-				$('#groupSelect').selectpicker();
+				if (uId == "0") {
+					for (var i = data.data.length - 1; i >= 0; i--) {
+						var group = data.data[i];
+						var temp = "<option value='" + group.GROUP_ID + "'>" + group.DESCRIPTION + "</option>";
+						$("#groupSelect").append(temp);
+					};
+					$('#groupSelect').selectpicker();
+				}else{
+					$("#groupSelect").empty();
+					$("#groupSelect").append(' <option value="0">-- 请选择权限组 --</option>');
+					var temp;
+					for (var i = data.data.length - 1; i >= 0; i--) {
+						var group = data.data[i];
+						$.ajax({
+							url:'/users/'+uId+'/securitygroups',
+							type:'get',
+							async   : false,
+							success:function (data2) {
+								if (data2.statusCode === 0) {
+									var datas = new Array();
+									for (var i = data2.data.length - 1; i >= 0; i--) {
+										var userGroup = data2.data[i];
+										datas[i] = userGroup.GROUP_ID;
+									};
+									if(datas.length){
+										if (group.GROUP_ID.in_array(datas)) {
+											 temp = "<option disabled='disabled' value='" + group.GROUP_ID + "'>" + group.DESCRIPTION + "</option>";
+										}else{
+											 temp = "<option value='" + group.GROUP_ID + "'>" + group.DESCRIPTION + "</option>";
+										}
+									}else{
+										 temp = "<option value='" + group.GROUP_ID + "' disabled='disabled'>" + group.DESCRIPTION + "</option>";
+									}
+									
+								};
+							}
+						})
+						
+						$("#groupSelect").append(temp);
+						$('#groupSelect').selectpicker('refresh');
+					};
+					$('#groupSelect').selectpicker();
+
+				}
 			};
 		}
 	})
 }
-
 function allocGroup () {
 
 	if($("#userSelect").val() == '0' ){
