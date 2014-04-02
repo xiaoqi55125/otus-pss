dcf0c88679811b075c70d4b638d3aa080a795413
Zhicheng Liu <xiaoqi55125@gmail.com>
2014-04-02 23:38:00 +0800

    add tips

--- public/javascript/pStockIn.js
+++ public/javascript/pStockIn.js
@@ -21,21 +21,26 @@ function getAllPreStockInList() {
 		success:function (data) {
 			if (data.statusCode === 0) {
 				var tempStr = data.data;
-				$("#add_listView").html("");
-				for (var i = 0; i < tempStr.length; i++) {
-					var cellData = tempStr[i];
-					var row = tdCont.row(cellData.PSI_ID);
-					var cellTime = tdCont.cell(moment(cellData.PSI_TIME).format("YYYY年 M月 D日 H:mm:ss"));
-					var cellOpeator = tdCont.cell(cellData.OPERATOR);
-					var cellRemark = tdCont.cell(cellData.REMARK);
-					var cellDetails = tdCont.cell($("<a href='javascript:void(0);'>查看详情</a>"));
-					cellDetails.click(tdCont.proStockDetail(cellData.PSI_ID));
-					row.append(cellTime);
-					row.append(cellOpeator);
-					row.append(cellRemark);
-					row.append(cellDetails);
-					$("#add_listView").append(row);
+				if (!tempStr.length) {
+					$("#add_listView").html("<h4>暂无记录!</h4>");
+				}else{
+					$("#add_listView").html("");
+					for (var i = 0; i < tempStr.length; i++) {
+						var cellData = tempStr[i];
+						var row = tdCont.row(cellData.PSI_ID);
+						var cellTime = tdCont.cell(moment(cellData.PSI_TIME).format("YYYY年 M月 D日 H:mm:ss"));
+						var cellOpeator = tdCont.cell(cellData.OPERATOR);
+						var cellRemark = tdCont.cell(cellData.REMARK);
+						var cellDetails = tdCont.cell($("<a href='javascript:void(0);'>查看详情</a>"));
+						cellDetails.click(tdCont.proStockDetail(cellData.PSI_ID));
+						row.append(cellTime);
+						row.append(cellOpeator);
+						row.append(cellRemark);
+						row.append(cellDetails);
+						$("#add_listView").append(row);
+					}
 				}
+				
 			}
 		}
 	})
@@ -81,9 +86,12 @@ function submitPreStockIn (psId) {
 		type:'post',
 		success:function (data) {
 			if (data.statusCode ===0) {
+				bootbox.alert("<h4>确定入库成功!</h4>");
+				getAllPreStockInList();
 				$('#subpStockIn').modal('hide');
 			}else{
-				bootbox.alert("确定入库失败,请重试");
+				bootbox.alert("<h4>确定入库失败,请重试</h4>");
+				$('#subpStockIn').modal('hide');
 			}
 		}
 	})
